76f282e27e74fc73e6f42d23d1f2153a5df01d31
Manish Choudhary <manishchaudharyjee@gmail.com>
2021-05-15 21:23:29 +0530

    example01 funt prog

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 2;
+var number2 = 5;
+console.log(add(number1, number2));
---
+++ app.ts
@@ -0,0 +1,7 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+let number1 = 2;
+let number2 = 5;
+
+console.log(add(number1, number2));
